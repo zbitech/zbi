84a99c0be7e35b9661e67910e0bddbe02cab3693
alphegasolutions <alphegasolutions@gmail.com>
2024-01-27 03:41:54 -0500

    create constants

---
+++ zbi-db/src/constants.ts
@@ -0,0 +1,14 @@
+
+export class Constants {
+    static readonly REQUEST_ID = "requestid";
+    static readonly INSTANCE_ID = "instance";
+    static readonly PROJECT_ID = "project";
+    static readonly USER = "user";
+    static readonly PERMISSION = "permission";
+
+    static readonly USER_ID = "x-user-id";
+    static readonly OWNER_ID = "x-owner-id";
+//    static readonly USER_EMAIL = "useremail";
+//    static readonly USER = "user";
+    static readonly START = "start";
+}
